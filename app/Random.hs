b5b9bd8dccd80e945d5a0c05c177bb26fecca5e9
Luke Iannini <lukexi@me.com>
2016-05-04 20:36:43 -0700

    colorHSL

--- app/Random.hs
+++ app/Random.hs
@@ -6,7 +6,7 @@ import Control.Monad.Trans
 import Types
 
 randomShapeState :: MonadIO m => m ShapeState
-randomShapeState = ShapeState 
+randomShapeState = ShapeState
       <$> randomPose
       <*> randomColor
       <*> randomScale
@@ -15,23 +15,23 @@ randomRIO' :: (Random a, MonadIO m) => (a,a) -> m a
 randomRIO' = liftIO . randomRIO
 
 randomPose :: MonadIO m => m (Pose GLfloat)
-randomPose = Pose 
+randomPose = Pose
   <$> randomPosition
   <*> randomOrientation
 randomPosition :: MonadIO m => m (V3 GLfloat)
-randomPosition = V3 
+randomPosition = V3
   <$> randomRIO' (-10, 10)
   <*> randomRIO' (-10, 10)
   <*> randomRIO' (-10, 10)
 
 randomScale :: MonadIO m => m (V3 GLfloat)
-randomScale = V3 
+randomScale = V3
   <$> randomRIO' (0, 10)
   <*> randomRIO' (0, 10)
   <*> randomRIO' (0, 10)
 
 randomAxis :: MonadIO m => m (V3 GLfloat)
-randomAxis = V3 
+randomAxis = V3
   <$> randomRIO' (0, 1)
   <*> randomRIO' (0, 1)
   <*> randomRIO' (0, 1)
@@ -40,15 +40,13 @@ randomOrientation :: MonadIO m => m (Quaternion GLfloat)
 randomOrientation = axisAngle <$> randomAxis <*> randomRIO' (-10, 10)
 
 randomColor :: MonadIO m => m (V4 GLfloat)
-randomColor = hslColor 
-  <$> randomRIO' (0,1) 
-  <*> randomRIO' (0,1) 
-  <*> randomRIO' (0,1) 
-  <*> pure 1
+randomColor = colorHSL
+  <$> randomRIO' (0,1)
+  <*> randomRIO' (0,1)
+  <*> randomRIO' (0,1)
 
 randomColorWithHue :: MonadIO m => GLfloat -> m (V4 GLfloat)
-randomColorWithHue hue = hslColor 
-  <$> pure hue 
-  <*> randomRIO' (0,1) 
-  <*> randomRIO' (0,1) 
-  <*> pure 1
+randomColorWithHue hue = colorHSL
+  <$> pure hue
+  <*> randomRIO' (0,1)
+  <*> randomRIO' (0,1)
