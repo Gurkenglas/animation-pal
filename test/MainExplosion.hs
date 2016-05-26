a4348ed2cb1cb92254bc216054023e7341eaed00
Luke Iannini <lukexi@me.com>
2016-05-25 18:33:00 -0700

    Fix explosion test

---
+++ test/MainExplosion.hs
@@ -0,0 +1,106 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE LambdaCase #-}
+
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+module Main where
+
+import Graphics.VR.Pal
+import Graphics.GL.Pal
+import Control.Monad.State.Strict
+import Control.Lens.Extra
+
+import Halive.Utils
+import Data.Time
+
+import Animation.Pal
+
+import Types
+import Render
+import Random
+
+data Moment = Moment
+  { momPitch :: !Float
+  , momHue   :: !Float
+  , momScale :: !Float
+  }
+
+data Pattern = Pattern
+  { ptnPitch :: [Float]
+  , ptnHue   :: [Float]
+  , ptnScale :: [Float]
+  }
+
+spawnCube :: (MonadState World m, MonadIO m) => m ()
+spawnCube = do
+
+  startTime <- utctDayTime <$> liftIO getCurrentTime
+
+  let hue = sin . realToFrac $ startTime
+      x = (* 10) . sin . (* 2) . realToFrac $ startTime
+      y = (* 10) . cos . (* 2) . realToFrac $ startTime
+  color <- randomColorWithHue hue
+
+  toShapeState <- ShapeState
+      <$> (randomPose <&> posPosition +~ V3 x y 0)
+      <*> randomColorWithHue hue
+      <*> pure 0
+
+  let shapeAnim = Animation
+        { animStart    = startTime
+        , animDuration = 1
+        , animFunc = shapeStateAnim
+        , animFrom = newShapeState
+                      & rndrColor .~ color
+                      & rndrPose . posOrientation .~ (axisAngle (V3 0 1 0) 0)
+                      & rndrPose . posPosition . _x .~ x
+                      & rndrPose . posPosition . _y .~ y
+        , animTo = toShapeState
+        }
+  wldAnimations <>= [shapeAnim]
+
+main :: IO ()
+main = do
+    vrPal@VRPal{..} <- reacquire 0 $ initVRPal "VRPal" []
+
+    -- Set up our cube resources
+    cubeProg <- createShaderProgram "test/cube.vert" "test/cube.frag"
+    cubeGeo  <- cubeGeometry (1 :: V3 GLfloat) (V3 1 1 1)
+    shape    <- makeShape cubeGeo cubeProg
+
+    glEnable GL_DEPTH_TEST
+    glClearColor 0 0 0.1 1
+
+    useProgram (sProgram shape)
+
+
+    let world = World (newPose {_posPosition = V3 0 0 10}) []
+
+    onSpawnTimer <- makeTimer 0.001
+
+    void . flip runStateT world . whileWindow gpWindow $ do
+        -- applyMouseLook gpWindow wldPlayer
+        applyWASD gpWindow wldPlayer
+        player <- use wldPlayer
+        (headM44, events) <- tickVR vrPal (transformationFromPose player)
+        forM_ events $ \case
+            GLFWEvent e -> do
+                closeOnEscape gpWindow e
+                applyGamepadJoystickMovement e wldPlayer
+                onKeyDown e Key'E $ replicateM_ 100 spawnCube
+            _ -> return ()
+
+        onSpawnTimer spawnCube
+
+        now <- utctDayTime <$> liftIO getCurrentTime
+
+        (shapeStates, newAnims, _finishedAnims) <- evalAnimations now <$> use wldAnimations
+        wldAnimations .= newAnims
+
+        renderWith vrPal headM44 $ \projM44 viewM44 -> do
+            glClear (GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT)
+            render shape shapeStates projM44 viewM44
+
+
+
--- test/Render.hs
+++ test/Render.hs
@@ -5,12 +5,9 @@ module Render where
 import Graphics.GL.Pal
 import Control.Monad.State
 import Control.Lens.Extra
-
-import Data.Maybe
-
 import Types
 
-render :: (MonadIO m, MonadState World m) 
+render :: (MonadIO m, MonadState World m)
        => Shape Uniforms
        -> [ShapeState]
        -> M44 GLfloat
@@ -20,7 +17,7 @@ render shape shapeStates projection viewMat = do
   let Uniforms{..} = sUniforms shape
       projectionView = projection !*! viewMat
       -- We extract eyePos from the view matrix to get Oculus offsets baked in
-      eyePos = fromMaybe viewMat (inv44 viewMat) ^. translation
+      eyePos = inv44 viewMat ^. translation
 
   uniformV3 uCamera eyePos
 
@@ -29,20 +26,20 @@ render shape shapeStates projection viewMat = do
     forM_ shapeStates $ \r -> do
       uniformV4 uDiffuse (r ^. rndrColor)
 
-      let model = mkTransformation (r ^. rndrPose . posOrientation) (r ^. rndrPose . posPosition) 
+      let model = mkTransformation (r ^. rndrPose . posOrientation) (r ^. rndrPose . posPosition)
                   !*! scaleMatrix (r ^. rndrScale)
 
       drawShape' model projectionView shape
 
 drawShape' :: MonadIO m => M44 GLfloat -> M44 GLfloat -> Shape Uniforms -> m ()
-drawShape' model projectionView shape = do 
+drawShape' model projectionView shape = do
 
   let Uniforms{..} = sUniforms shape
 
-  uniformM44 uInverseModel        (fromMaybe model (inv44 model))
+  uniformM44 uInverseModel        (inv44 model)
   uniformM44 uModel               model
 
   uniformM44 uModelViewProjection (projectionView !*! model)
 
-  let vc = geoVertCount (sGeometry shape)
+  let vc = geoIndexCount (sGeometry shape)
   glDrawElements GL_TRIANGLES vc GL_UNSIGNED_INT nullPtr
